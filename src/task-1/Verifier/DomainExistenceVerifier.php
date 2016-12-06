28942fc22d591fe4c404743e0b20ff4cf1830b86
Guba Andrei <xg.andrew@gmail.com>
2016-12-06 11:55:22 +1000

    impl domain existence verifier

--- src/task-1/Verifier/DomainExistenceVerifier.php
+++ src/task-1/Verifier/DomainExistenceVerifier.php
@@ -2,10 +2,16 @@
 
 namespace Verifier;
 
-class DomainExistenceVerifier implements VerifierInterface 
+use Verifier\Exception\DomainNotFoundException;
+
+class DomainExistenceVerifier implements VerifierInterface
 {
     public function verify($email)
     {
-        // TODO: Implement verify() method.
+        list(, $domain) = explode('@', $email) + array(null, null);
+
+        if ($domain && !filter_var(gethostbyname($domain), FILTER_VALIDATE_IP)) {
+            throw new DomainNotFoundException(sprintf('Cannot resolve domain <%s>.'));
+        }
     }
 }
