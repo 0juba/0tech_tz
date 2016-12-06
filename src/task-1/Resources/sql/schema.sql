3bd04635715dc3fb696977401416789f40fc9c62
Guba Andrei <xg.andrew@gmail.com>
2016-12-06 16:19:39 +1000

    added schema sql

---
+++ src/task-1/Resources/sql/schema.sql
@@ -0,0 +1,37 @@
+CREATE DATABASE IF NOT EXISTS demo
+  DEFAULT CHARACTER SET utf8
+  DEFAULT COLLATE utf8_general_ci;
+
+CREATE TABLE IF NOT EXISTS email(
+  id INT AUTO_INCREMENT,
+  email VARCHAR(255) UNIQUE NOT NULL,
+  PRIMARY KEY (id)
+) ENGINE=INNODB;
+
+-- ------------------------------
+-- Уточнение в задаче, что домены могут быть одинаковыми для адресов и их много навеяло вот такую схему:
+
+CREATE TABLE IF NOT EXISTS email(
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(64) NOT NULL,
+  domain_id INT NOT NULL,
+
+  PRIMARY KEY (id),
+
+  INDEX domain_id_idx(domain_id),
+  UNIQUE INDEX email_unq_idx(username, domain_id),
+
+  FOREIGN KEY (domain_id) REFERENCES domain(id) ON DELETE CASCADE
+) ENGINE=INNODB;
+
+CREATE TABLE IF NOT EXISTS domain(
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  domain VARCHAR(255) UNIQUE NOT NULL
+) ENGINE=INNODB;
+
+-- Чего я НЕ сообразил - как это можно использовать? Если только для более удобного сбора каких-то данных, например, сколько у нас
+-- адресов принадлежат такому-то домену ...
+-- Например:
+SELECT COUNT(d.domain), d.domain FROM domain d
+INNER JOIN email e ON e.domain_id = d.id
+GROUP BY d.domain
