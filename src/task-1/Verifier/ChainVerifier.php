6c232acbcbcee5d085591e1079493a93f3c0fd1c
Guba Andrei <xg.andrew@gmail.com>
2016-12-06 11:36:01 +1000

    impl chain verifier

---
+++ src/task-1/Verifier/ChainVerifier.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Verifier;
+
+class ChainVerifier implements VerifierInterface
+{
+    /**
+     * @var array
+     */
+    private $verifiers;
+
+    public function __construct(array $verifiers)
+    {
+        array_walk($verifiers, function ($verifier) {
+            if (!$verifier instanceof VerifierInterface) {
+                throw new \InvalidArgumentException('Verifier have to implement Verifier\VerifierInterface');
+            }
+        });
+
+        $this->verifiers = $verifiers;
+    }
+
+    public function verify($value)
+    {
+        foreach ($this->verifiers as $verifier) {
+                $verifier->verify($value);
+        }
+    }
+}
